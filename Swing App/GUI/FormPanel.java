a730b6b1299c3cd0c847f44344cee47cdef37699
unicoded <44432228+unicoded@users.noreply.github.com>
2019-07-07 18:48:26 +0300

    Add files via upload

---
+++ Swing App/GUI/FormPanel.java
@@ -0,0 +1,319 @@
+package GUI;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+public class FormPanel extends JPanel {
+
+	private JLabel nameLabel;
+	private JLabel occupationLabel;
+	private JTextField nameField;
+	private JTextField occupationField;
+	private JButton okBtn;
+	private FormListener formListener;
+	private JList ageList;
+	private JComboBox empCombo;
+	private JCheckBox citizenCheck;
+	private JTextField CitizenshipField;
+	private JLabel CitizenshipLabel;
+	//radio buttons
+	private JRadioButton maleRadio;
+	private JRadioButton femaleRadio;
+	private ButtonGroup genderGroup;
+	//MENU
+	
+	
+
+	public FormPanel() {
+		Dimension dim = getPreferredSize();
+		dim.width = 250;
+		setPreferredSize(dim);
+
+		okBtn = new JButton("OK");// OK Buton
+		
+		nameLabel = new JLabel("Name: ");
+		occupationLabel = new JLabel("Occupation: ");
+		nameField = new JTextField(10);
+		occupationField = new JTextField(10);
+		ageList = new JList();
+		
+		empCombo = new JComboBox();
+		
+		citizenCheck = new JCheckBox();
+		CitizenshipField=new JTextField(10);
+		CitizenshipLabel=new JLabel("Country: ");
+		
+		maleRadio=new JRadioButton("male");
+		femaleRadio=new JRadioButton("female");
+		maleRadio.setActionCommand("Male");
+		femaleRadio.setActionCommand("Female");
+		genderGroup=new ButtonGroup();
+		
+		//set up gender radios
+		genderGroup.add(maleRadio);
+		genderGroup.add(femaleRadio);
+		
+		//set up tax ID
+		CitizenshipLabel.setEnabled(false);
+		CitizenshipField.setEnabled(false);
+		
+		//set up mnemonix
+		okBtn.setMnemonic(KeyEvent.VK_O);
+		//from labels
+		nameLabel.setDisplayedMnemonic(KeyEvent.VK_N);
+		nameLabel.setLabelFor(nameField);
+		
+		citizenCheck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				boolean isTicked=citizenCheck.isSelected();
+				CitizenshipLabel.setEnabled(isTicked);
+				CitizenshipField.setEnabled(isTicked);
+			}
+			
+		});
+
+		// set up list box
+		DefaultListModel ageModel = new DefaultListModel();
+		ageModel.addElement(new AgeCategory(0, "0: Under 18"));
+		ageModel.addElement(new AgeCategory(1, "1: 18 to 65"));
+		ageModel.addElement(new AgeCategory(2, "2: Over 65"));
+		ageList.setModel(ageModel);
+
+		// set up combobox
+		DefaultComboBoxModel empModel = new DefaultComboBoxModel();
+		empModel.addElement("Employed");
+		empModel.addElement("Self-employed");
+		empModel.addElement("Unemployed");
+		empCombo.setModel(empModel);
+		empCombo.setEditable(true);
+
+		ageList.setPreferredSize(new Dimension(110, 70));
+		ageList.setBorder(BorderFactory.createEtchedBorder());
+		ageList.setSelectedIndex(1);
+		
+		
+        //FormEvent Listener
+		okBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = nameField.getText();
+				String occupation = occupationField.getText();
+				AgeCategory ageCat = (AgeCategory) ageList.getSelectedValue();
+				String empCat = (String) empCombo.getSelectedItem();
+				String Country=CitizenshipField.getText();
+				boolean usCitizen=citizenCheck.isSelected();
+				
+				if(maleRadio.isSelected()||femaleRadio.isSelected()) {
+					
+					String genderCommand=genderGroup.getSelection().getActionCommand();
+					
+					if(Country.isEmpty()) {
+						Country="Not specified";
+					}
+					
+					FormEvent ev = new FormEvent(this, name, occupation, ageCat.getID(), empCat,Country,usCitizen,genderCommand);
+					
+					if (formListener != null) {
+						formListener.formEventOccured(ev);
+					}
+				}else {
+					JOptionPane.showMessageDialog(FormPanel.this, "Gender was not selected", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+
+		});
+
+		TitledBorder innerBorder = BorderFactory.createTitledBorder("Add Person");
+		javax.swing.border.Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+
+		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+
+		layoutComponets();
+	}
+
+	public void layoutComponets() {
+		setLayout(new GridBagLayout());
+
+		GridBagConstraints gc = new GridBagConstraints();
+
+		/////////// first row////////////////
+
+		gc.gridy = 0;
+
+		gc.weightx = 1;
+		gc.weighty = 0.1;
+
+		gc.gridx = 0;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(nameLabel, gc);
+
+		gc.gridx = 1;
+		gc.insets = new Insets(0, 0, 0, 0);
+		gc.anchor = GridBagConstraints.LINE_START;
+		add(nameField, gc);
+
+		/////////// first row////////////////
+
+		/////////// second row//////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 0.1;
+
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(occupationLabel, gc);
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(occupationField, gc);
+
+		/////////// second row//////////////
+
+		/////////// third row/////////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 0.2;
+
+		gc.gridx = 0;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(new JLabel("Age Category: "), gc);
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(ageList, gc);
+
+		/////////// fourth row/////////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 0.2;
+
+		gc.gridx = 0;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(new JLabel("Employment: "), gc);
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(empCombo, gc);
+		
+        /////////// next row/////////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 0.2;
+
+		gc.gridx = 0;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(new JLabel("Add Citizenship: "), gc);
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(citizenCheck, gc);
+
+		
+		 /////////// next row/////////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 0.2;
+
+		gc.gridx = 0;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(CitizenshipLabel, gc);
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(CitizenshipField, gc);
+		
+        /////////// next row/////////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 0.01;
+
+		gc.gridx = 0;
+		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(new JLabel("Genger: "), gc);
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(maleRadio, gc);
+		
+        /////////// next row/////////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 0.2;
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(femaleRadio, gc); 
+		
+		
+		/////////// button row/////////////////
+
+		gc.gridy++;
+
+		gc.weightx = 1;
+		gc.weighty = 3.0;
+
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(okBtn, gc);
+	}
+
+	public void setFormListener(FormListener listener) {
+		this.formListener = listener;
+	}
+
+}
+
+class AgeCategory {
+	private String text;
+	private int id;
+
+	public AgeCategory(int id, String text) {
+		this.id = id;
+		this.text = text;
+	}
+
+	public String toString() {
+		return text;
+	}
+
+	public int getID() {
+		return id;
+	}
+}
